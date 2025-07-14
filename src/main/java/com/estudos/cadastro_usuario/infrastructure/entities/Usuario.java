7c91e90f17a1a5549220f16230f490fb581b5431
joaovpt <joaotassi18@gmail.com>
2025-07-14 13:22:59 -0300

    feat: add Usuario entity for user management

---
+++ src/main/java/com/estudos/cadastro_usuario/infrastructure/entities/Usuario.java
@@ -0,0 +1,27 @@
+package com.estudos.cadastro_usuario.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+
+
+
+}
